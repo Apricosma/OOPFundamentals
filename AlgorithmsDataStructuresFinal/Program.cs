b9681228883756a42b75412417374f632552a4c1
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-20 09:22:05 -0600

    Add initializeCoins()

--- AlgorithmsDataStructuresFinal/Program.cs
+++ AlgorithmsDataStructuresFinal/Program.cs
@@ -1,17 +1,27 @@
-﻿using System.ComponentModel;
-
+﻿
 class Program
 {
     static Dictionary<string, int> initializeCoins()
     {
-        Dictionary<string, int> = new Dictionary<string, int>>();
+        // create a dictionary with all the coins as a string, and in integer to keep track of the amount
+        Dictionary<string, int> makeCoins = new Dictionary<string, int>();
+        makeCoins.Add("$1", 17);
+        makeCoins.Add("$2", 12);
+        makeCoins.Add("$5", 10);
+        makeCoins.Add("$10", 8);
+        makeCoins.Add("$20", 3);
 
-        return initializeCoins();
+        return makeCoins;
     }
 
 
     static void Main(string[] args)
     {
+        Dictionary<string, int> StoredCoins = initializeCoins();
 
+        foreach(KeyValuePair<string, int> pair in StoredCoins)
+        {
+            Console.WriteLine($"{pair.Key} : {pair.Value}");
+        }
     }
 }
