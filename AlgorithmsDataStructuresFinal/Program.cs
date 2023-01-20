445e95c73ec43089491b7ddd942be40bc32aaaa0
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-20 09:14:38 -0600

    Add project files.

---
+++ AlgorithmsDataStructuresFinal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+class Program
+{
+    static Dictionary<string, int> initializeCoins()
+    {
+        Dictionary<string, int> = new Dictionary<string, int>>();
+
+        return initializeCoins();
+    }
+
+
+    static void Main(string[] args)
+    {
+
+    }
+}
