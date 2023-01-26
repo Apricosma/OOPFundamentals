6a5d4293adfcd5fa75348497c48c6a0a88f864e1
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-26 15:56:33 -0600

    Initial commit

---
+++ Lab2/CarPark.cs
@@ -0,0 +1,63 @@
+﻿using Lab2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementDemo
+{
+    public class CarPark
+    {
+        private HashSet<ParkingSpot> _parkingSpots = new HashSet<ParkingSpot>();
+
+        // expose specific methods on the collection, not the entire collection
+        public void AddParkingSpot(ParkingSpot parkingSpot)
+        {
+            _parkingSpots.Add(parkingSpot);
+            // bi-directional relationship -- set this CarPark as the parking spot's property
+            parkingSpot.SetCarPark(this);
+        }
+
+        // expose a COPY of our list (not a reference)
+        public HashSet<ParkingSpot> GetParkingSpots()
+        {
+            HashSet<ParkingSpot> spotReferenceCopies;
+
+            // toHashSet returns a HashSet of Elements stored in a collection
+            // it does not return the original collection
+            spotReferenceCopies = _parkingSpots.ToHashSet();
+
+            // create a new hashset of the parking spots that is a copy of the original hashset, so that the original cannot be modified with this Get Method
+            return spotReferenceCopies;
+        }
+
+        private int _capacity;
+        public int Capacity { get { return _capacity; } }
+        private void _setCapacity(int newCapacity)
+        {
+            if (newCapacity > 0)
+            {
+                _capacity = newCapacity;
+            }
+            else
+            {
+                throw new Exception("Capacity must exceed 0");
+            }
+        }
+
+        private void _initializeEmptySpots()
+        {
+            for (int i = 1; i <= Capacity; i++)
+            {
+                _parkingSpots.Add(new ParkingSpot(i, this));
+            }
+        }
+        private int _spotCount = 1;
+        public CarPark(int capacity)
+        {
+            _setCapacity(capacity);
+            //_initializeEmptySpots();
+        }
+    }
+}
---
+++ Lab2/ParkingSpots.cs
@@ -0,0 +1,47 @@
+﻿using Lab2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementDemo
+{
+    // ParkingSpot tracks the relationship between Vehicles and CarParks
+    // many-to-many relationship
+    // vehicles can be parked at many different carparks
+    // carparks can store many different vehicles
+    public class ParkingSpot
+    {
+        private int _number;
+        private Vehicle _vehicle;
+        private CarPark _carPark;
+
+        // exposes the CarPark object on the ParkingSpot
+        // but we have total control over what is exposed on the CarPark object itself
+        public CarPark CarPark { get { return _carPark; } }
+
+        // exposes the _carPark value to be set
+        public void SetCarPark(CarPark carPark)
+        {
+            _carPark = carPark;
+        }
+
+        public Vehicle Vehicle
+        {
+            get { return _vehicle; }
+            set { _vehicle = value; }
+        }
+
+        public ParkingSpot(int number, CarPark carPark)
+        {
+            _number = number;
+            _carPark = carPark;
+        }
+
+        public ParkingSpot(int number)
+        {
+            _number = number;
+        }
+    }
+}
